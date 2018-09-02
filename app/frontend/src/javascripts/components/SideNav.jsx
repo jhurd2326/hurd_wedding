ab6450e3a95bd071129f90f3b705803a57b4bfab
Jake Hurd <jake.hurd@gmail.com>
2018-09-01 23:32:39 -0400

    Add side nav and background image

--- app/frontend/src/javascripts/components/SideNav.jsx
+++ app/frontend/src/javascripts/components/SideNav.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import {
   IconButton,
-  Drawer
+  Drawer,
+  List,
+  ListItem,
+  ListItemText
 } from '@material-ui/core'
 
 import MenuIcon from '@material-ui/icons/Menu';
@@ -23,15 +26,29 @@ export default class SideNav extends React.Component {
 
   render () {
     return (
-      <React.Fragment>
-        <IconButton onClick={this.toggleNav}>
+      <div className="h-0">
+        <IconButton onClick={this.toggleNav} className="menu-button">
           <MenuIcon/>
         </IconButton>
 
         <Drawer open={this.state.isOpen} onClose={this.toggleNav}>
-          Test
+          <List disablePadding={true} classes={{root: 'side-nav'}}>
+            <div className="nav-logo"/>
+            <ListItem button={true}>
+              <ListItemText>Home</ListItemText>
+            </ListItem>
+            <ListItem button={true}>
+              <ListItemText>Our Story</ListItemText>
+            </ListItem>
+            <ListItem button={true}>
+              <ListItemText>The Venue</ListItemText>
+            </ListItem>
+            <ListItem button={true}>
+              <ListItemText>RSVP</ListItemText>
+            </ListItem>
+          </List>
         </Drawer>
-      </React.Fragment>
+      </div>
     );
   }
 }
