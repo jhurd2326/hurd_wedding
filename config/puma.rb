afb9344fd6ee30f64b8c11fa211bdc30350fbbb3
Jake Hurd <jake.hurd@gmail.com>
2018-09-30 01:53:00 -0400

    Fix up puma config and favicon

--- config/puma.rb
+++ config/puma.rb
@@ -45,11 +45,11 @@ if ENV["RAILS_ENV"] == "production"
 
   pidfile "#{shared_dir}/pids/puma.pid"
   state_path "#{shared_dir}/pids/puma.state"
-  activate_control_app
 
   on_worker_boot do
-    require "active_record"
-    ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-    ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+    ActiveRecord::Base.establish_connection
   end
+
+  prune_bundler
+  activate_control_app "unix://#{shared_dir}/sockets/puma.sock"
 end
