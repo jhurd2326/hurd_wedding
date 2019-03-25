fb1520e3990dbf01ffaa3c9214d96409800cb475
Jake Hurd <jake.hurd@gmail.com>
2019-03-24 21:35:55 -0400

    Upgrade to Webpacker 4

--- Gemfile
+++ Gemfile
@@ -19,7 +19,7 @@ gem "sass-rails", "~> 5.0"
 gem "turbolinks", "~> 5"
 gem "tzinfo-data", platforms: %i[mingw mswin x64_mingw jruby]
 gem "uglifier", ">= 1.3.0"
-gem "webpacker", "~> 3.5"
+gem "webpacker"
 
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,6 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 
-module.exports = environment
-
 environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
@@ -11,3 +9,8 @@ environment.plugins.prepend(
     jquery: 'jquery'
   })
 )
+
+// https://github.com/rails/webpacker/blob/master/docs/v4-upgrade.md#excluding-node_modules-from-being-transpiled-by-babel-loader
+environment.loaders.delete('nodeModules');
+
+module.exports = environment
